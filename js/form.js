d304f6cef99f4267c8ae97461f621374b8b03335
Vitor <vitorrossignolli@gmail.com>
2020-02-05 20:30:21 -0300

    Refatoração
    First Commit

--- js/form.js
+++ js/form.js
@@ -2,18 +2,34 @@ var addPaciente = document.querySelector("#adicionar-paciente");
 
 addPaciente.addEventListener("click", poup);
 
-function poup(){ event.preventDefault();
+
+
+
+
+function poup() {
+	event.preventDefault();
+
 	var form = document.querySelector("#form-adiciona");
-	var nome = form.nome.value;
+	var teste = form.textContent;
+	console.log(teste);
+	var paciente = GetDadosPacienteForm(form);
+	console.log(paciente);
+
+	var nome = form.nomes.value;
 	var peso = form.peso.value;
 	var altura = form.altura.value;
 	var gordura = form.gordura.value;
+
+
+
 	var pacienteTr = document.createElement("tr");
 	var nomeTd = document.createElement("td");
 	var pesoTd = document.createElement("td");
 	var alturaTd = document.createElement("td");
 	var gorduraTd = document.createElement("td");
 	var imcTd = document.createElement("td");
+
+
 	nomeTd.textContent = nome;
 	pesoTd.textContent = peso;
 	alturaTd.textContent = altura;
@@ -24,6 +40,8 @@ function poup(){ event.preventDefault();
 	pacienteTr.appendChild(alturaTd);
 	pacienteTr.appendChild(gorduraTd);
 	pacienteTr.appendChild(imcTd);
+
+
 	var tabela = document.querySelector("#tabela-pacientes");
 	tabela.appendChild(pacienteTr);
 
@@ -38,3 +56,18 @@ function poup(){ event.preventDefault();
 
 
 }
+
+function GetDadosPacienteForm(form) {
+
+	var paciente = {
+		nome: form.nomes.value,
+		peso: form.peso.value,
+		altura: form.altura.value,
+		gordura: form.gordura.value,
+		imc: calcularImc(form.peso.value, form.altura.value)
+	}
+
+	return paciente;
+
+}
+
